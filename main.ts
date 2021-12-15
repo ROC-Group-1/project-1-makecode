c4fa0abf461b833fec0aa1ae627eb35956a07f9f
SMJS <38814077+SMJSGaming@users.noreply.github.com>
2021-12-15 13:42:38 +0100

    Partial encounter implementation

--- main.ts
+++ main.ts
@@ -6,6 +6,54 @@ namespace SpriteKind {
     export const NPC_Top = SpriteKind.create()
     export const NPC_Bottom = SpriteKind.create()
     export const Bush = SpriteKind.create()
+    export const cursor = SpriteKind.create()
+    export const button = SpriteKind.create()
+}
+// makes the player use a treat, wich makes it easier to catch, but more likely to run
+function useTreat () {
+    textSprite = textsprite.create("You used a treat", 1, 15)
+    textSprite.setPosition(50, 100)
+    aggravation += -1
+    timer.after(2500, function () {
+        textSprite.destroy()
+        berry = sprites.create(img`
+            . . . . . . . . 
+            . . . . . . . . 
+            . . . . 7 . . . 
+            . . . . 3 7 . . 
+            . . . 3 3 3 7 . 
+            . . 3 3 3 . . . 
+            . . 3 3 . . . . 
+            . . . . . . . . 
+            `, SpriteKind.Projectile)
+        berry.setPosition(25, 69)
+        berry.setVelocity(200, -84)
+        timer.after(300, function () {
+            berry.destroy()
+            enemyTurn()
+        })
+    })
+}
+function getCreature () {
+    creature = sprites.create(img`
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        `, SpriteKind.Enemy)
+    creature.setPosition(104, 35)
 }
 function getSpriteLocation (sprite: Sprite) {
     return getLocation(sprite.x, sprite.y)
@@ -66,6 +114,86 @@ function playerHasStopped () {
     }
     rotation = ""
 }
+// throw a rock to deal damage, making it harder to catch, but less likely to run
+function useRock () {
+    timer.after(2500, function () {
+        textSprite.destroy()
+        if (_throw < 15) {
+            throwableRock = sprites.create(img`
+                . b b b b b . . 
+                b b b b b b b . 
+                b b b b b b b b 
+                b b b b b b b b 
+                b b b b b b b b 
+                b b b b b b b b 
+                . b b b b b b . 
+                . . b b b b . . 
+                `, SpriteKind.Projectile)
+            throwableRock.setPosition(25, 69)
+            throwableRock.setVelocity(200, -150)
+            timer.after(300, function () {
+                throwableRock.destroy()
+                textSprite = textsprite.create("The Rock missed", 1, 15)
+                textSprite.setPosition(50, 100)
+                timer.after(2500, function () {
+                    textSprite.destroy()
+                    enemyTurn()
+                })
+            })
+        } else {
+            aggravation += 1
+            throwableRock = sprites.create(img`
+                . b b b b b . . 
+                b b b b b b b . 
+                b b b b b b b b 
+                b b b b b b b b 
+                b b b b b b b b 
+                b b b b b b b b 
+                . b b b b b b . 
+                . . b b b b . . 
+                `, SpriteKind.Projectile)
+            throwableRock.setPosition(25, 69)
+            throwableRock.setVelocity(200, -84)
+            timer.after(300, function () {
+                throwableRock.destroy()
+                HP.value = HP.value - randint(10, 20)
+                if (HP.value == 0) {
+                    textSprite = textsprite.create("The opposing [enter name here] fainted", 1, 15)
+                    textSprite.setPosition(50, 100)
+                    creature.vy += 100
+                    timer.after(100, function () {
+                        creature.destroy()
+                    })
+                    timer.after(2000, function () {
+                        textSprite.destroy()
+                        timer.after(2000, function () {
+                            returnToMap()
+                        })
+                    })
+                } else {
+                    enemyTurn()
+                }
+            })
+        }
+    })
+    textSprite = textsprite.create("You used a rock", 1, 15)
+    textSprite.setPosition(50, 100)
+    _throw = randint(0, 100)
+}
+function turnStart () {
+    story.startCutscene(function () {
+        story.showPlayerChoices("Catch", "Treat", "Rock", "Run")
+        if (story.checkLastAnswer("Catch")) {
+            useBall()
+        } else if (story.checkLastAnswer("Treat")) {
+            useTreat()
+        } else if (story.checkLastAnswer("Rock")) {
+            useRock()
+        } else if (story.checkLastAnswer("Run")) {
+            justRun()
+        }
+    })
+}
 function startEncounter (monster: string) {
     controller.moveSprite(playerSprite, 0, 0)
     isMap = false
@@ -89,15 +217,72 @@ function startEncounter (monster: string) {
             scene.setBackgroundColor(15)
         })
     }
+    scene.centerCameraAt(0, 0)
     timer.after(1400, function () {
-        initMapScene(playerSprite.x, playerSprite.y)
+        character = sprites.create(img`
+            a a a a a a a a a a a a a a a a 
+            a a a a a a a a a a a a a a a a 
+            a a a a a a a a a a a a a a a a 
+            a a a a f a a a a a a f a a a a 
+            a a a a a a a a a a a a a a a a 
+            a a a a a a a a a a a a a a a a 
+            a a a a a a a a a a a a a a a a 
+            a a a a a a a a a a a a a a a a 
+            a a a a a a a a a a a a a a a a 
+            a a a a a a a a a a a a a a a a 
+            a a a f a a a a a a a a a f a a 
+            a a a a f f f f f f f f f a a a 
+            a a a a a a a a a a a a a a a a 
+            a a a a a a a a a a a a a a a a 
+            a a a a a a a a a a a a a a a a 
+            a a a a a a a a a a a a a a a a 
+            `, SpriteKind.Player)
+        character.setPosition(25, 69)
+        getCreature()
+        aggravation = 5
+        textSprite = textsprite.create("", 1, 15)
+        textSprite.setPosition(50, 100)
+        timer.after(2500, function () {
+            textSprite.destroy()
+            HP = statusbars.create(45, 4, StatusBarKind.EnemyHealth)
+            HP.max = 143
+            HP.value = 143
+            HP.setPosition(125, 17)
+            turnStart()
+        })
     })
 }
 function isDirectionPressed () {
     return controller.up.isPressed() || (controller.down.isPressed() || (controller.left.isPressed() || controller.right.isPressed()))
 }
+// make it return to the map here
+function justRun () {
+    if (randint(0, 100) < 5) {
+        story.printCharacterText("You couldn't get away")
+        enemyTurn()
+    } else {
+        story.printCharacterText("You got away savely")
+        returnToMap()
+    }
+}
 function getLocation (x: number, y: number) {
     return tiles.getTileLocation(Math.floor(x / 16), Math.floor(y / 16))
+}
+// make the enemy make its move
+function enemyTurn () {
+    timer.after(1000, function () {
+        if (aggravation * 10 + randint(0, 100) < 95) {
+            creature.vx += 100
+            timer.after(100, function () {
+                creature.destroy()
+            })
+            story.printCharacterText("The opposing [enter name here] fled")
+            returnToMap()
+        } else {
+            story.printCharacterText("The opposing [enter name here] is watching carefully")
+            turnStart()
+        }
+    })
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileGrass2, function (sprite, location) {
     newGrassLoc = getSpriteLocation(playerSprite)
@@ -115,6 +300,12 @@ scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileGrass2, function (spri
         lastGrassLoc = tiles.getTileLocation(0, 0)
     }
 })
+function returnToMap () {
+    character.destroy()
+    creature.destroy()
+    HP.destroy()
+    initMapScene(playerSprite.x, playerSprite.y)
+}
 function initMapScene (x: number, y: number) {
     initMap()
     isMap = true
@@ -126,12 +317,92 @@ function initMapScene (x: number, y: number) {
     scene.cameraFollowSprite(playerSprite)
     music.setVolume(100)
 }
-let isMap = false
+// makes the player throw the ball if selected
+function useBall () {
+    textSprite = textsprite.create("You used the Net Stone", 1, 15)
+    textSprite.setPosition(50, 100)
+    timer.after(2500, function () {
+        textSprite.destroy()
+        thrownNetStone = sprites.create(img`
+            . b b b b b . . 
+            b b b b b b b . 
+            b b b b b b b b 
+            b b b b b b b b 
+            b b b b b b b b 
+            b b b b b b b b 
+            . b b b b b b . 
+            . . b b b b . . 
+            `, SpriteKind.Projectile)
+        thrownNetStone.setPosition(25, 69)
+        thrownNetStone.setVelocity(200, -84)
+        timer.after(300, function () {
+            thrownNetStone.destroy()
+            creature.destroy()
+            netStone = sprites.create(img`
+                . b b b b b . . 
+                b b b b b b b . 
+                b b b b b b b b 
+                b b b b b b b b 
+                b b b b b b b b 
+                b b b b b b b b 
+                . b b b b b b . 
+                . . b b b b . . 
+                `, SpriteKind.Projectile)
+            netStone.setPosition(104, 35)
+        })
+        catchRate = aggravation * 10 + (randint(0, 100) + HP.value)
+        if (catchRate < 200) {
+            timer.after(5000, function () {
+                thrownNetStone = sprites.create(img`
+                    . d d d d d . . 
+                    d d d d d d d . 
+                    d d d d d d d d 
+                    d d d d d d d d 
+                    d d d d d d d d 
+                    d d d d d d d d 
+                    . d d d d d d . 
+                    . . d d d d . . 
+                    `, SpriteKind.Projectile)
+                textSprite = textsprite.create("[enter name here] was caught", 1, 15)
+                textSprite.setPosition(50, 100)
+            })
+            timer.after(10000, function () {
+                textSprite.destroy()
+            })
+            timer.after(12500, function () {
+                returnToMap()
+            })
+        } else {
+            timer.after(randint(7500, 10000), function () {
+                getCreature()
+                netStone.destroy()
+                textSprite = textsprite.create("[enter name here] broke free", 1, 15)
+                textSprite.setPosition(50, 100)
+                timer.after(2000, function () {
+                    textSprite.destroy()
+                    enemyTurn()
+                })
+            })
+        }
+    })
+}
+let catchRate = 0
+let netStone: Sprite = null
+let thrownNetStone: Sprite = null
+let character: Sprite = null
+let HP: StatusBarSprite = null
+let throwableRock: Sprite = null
+let _throw = 0
+let creature: Sprite = null
+let berry: Sprite = null
+let aggravation = 0
+let textSprite: TextSprite = null
 let staticKinds: number[] = []
 let monsters: string[] = []
-let isGlitching = false
 let isGrassHit = false
 let rotation = ""
+let isMap = false
+let isGlitching = false
 let npcLocation: tiles.Location = null
 let playerLocation: tiles.Location = null
 let newGrassLoc: tiles.Location = null
@@ -182,6 +453,7 @@ function npcInteraction(npc: NPCInfo, x: number, y: number) {
     let originalLoc = getLocation(originalCords[0], originalCords[1]);
     let originalImage = npc.sprite.image
     let goBackAnimation: Image[] = null
+    let afterCallback: () => void
     story.startCutscene(function () {
         isScene = true
         controller.moveSprite(playerSprite, 0, 0)
@@ -189,6 +461,15 @@ function npcInteraction(npc: NPCInfo, x: number, y: number) {
         story.spriteSayText(npc.sprite, npc.callOut, 15, 1, story.TextSpeed.VeryFast)
         if (npc.name == "Park Guard") {
             goBackAnimation = assets.animation`heroWalkLeft`
+            afterCallback = () => {
+                timer.after(300000 - 5000, function () {
+                    isGlitching = true
+                    if (isMap) {
+                        glitchInit()
+                    }
+                    info.startCountdown(300000)
+                })
+            }
             animation.runImageAnimation(npc.sprite, assets.animation`heroWalkRight`, 200, true)
         }
         story.spriteMoveToLocation(npc.sprite, x, y, 70)
@@ -196,6 +477,9 @@ function npcInteraction(npc: NPCInfo, x: number, y: number) {
         npc.sprite.setImage(originalImage)
         for (let dialog of npc.dialogs) {
             story.printCharacterText(dialog.text, dialog.name || npc.name)
+        }
+        if (afterCallback) {
+            afterCallback()
         }
         animation.runImageAnimation(
             npc.sprite,
@@ -236,12 +520,6 @@ SpriteKind.NPC_Top,
 SpriteKind.NPC_Bottom,
 SpriteKind.Bush
 ]
-timer.after(300000 - 5000, function () {
-    isGlitching = true
-    if (isMap) {
-        glitchInit()
-    }
-})
 initMapScene(160, 5)
 // effects
 forever(function () {
